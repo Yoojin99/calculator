9c14ce46b685846bdb5a23ae17299e211d893767
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2021-02-01 17:27:08 +0900

    #신입-베이스캠프-8기-루키오리TF/11 변수 이름을 의미있게 변경

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 {
 	int number1, number2, result;
 	char operator;
-	
+	 
 	printf("Please enter a expression");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
